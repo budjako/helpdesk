3f31f34a2f3fda6e2062e69c7523089fa10ca601
budjako <budjako@gmail.com>
2014-05-15 15:43:55 +0800

    MySQL documentation

--- staff/home.php
+++ staff/home.php
@@ -1,6 +1,6 @@
 <?php include_once("header.html") ?>
 
 <a href="views.php">View Tickets, Threads and FAQ</a><br />
-<a href="editfaq.php">Edit/View FAQ</a><br />
+<a href="editfaq.php">Edit/View/Delete FAQ</a><br />
 
 <?php include_once("footer.html") ?>
